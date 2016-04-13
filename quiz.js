c3b092d7fbd068f321bd70e2dac1988a01876381
jacobfrank96 <jacobfrank@jacobs-air.dhcp.wustl.edu>
2016-04-13 18:22:17 -0500

    commit

--- quiz.js
+++ quiz.js
@@ -14,11 +14,11 @@ if ( answer1.toUpperCase() === 'NETWORK LAYER' ) {
  correct += 1;
 }
 
-if ( answer2.toUpperCase() === 'Transport Layer' ) {
+if ( answer2.toUpperCase() === 'TRANSPORT LAYER' ) {
  correct += 1;
 }
 
-if ( answer3.toUpperCase() === 'Transport Layer' ) {
+if ( answer3.toUpperCase() === 'TRANSPORT LAYER' ) {
  correct += 1;
 }
 
@@ -31,4 +31,52 @@ if ( answer5.toUpperCase() === 'GETINPUTSTREAM AND CLOSE' ) {
 }
 
 // output results
-document.write("<p>write something to page...</p>");
+if ( answer1.toUpperCase() === 'NETWORK LAYER' ) {
+ correct += 1;
+ document.write("<p>" +"This was your answer: " + answer1+ "...GOOD LOOKS! that was correct"+"</p>");
+}
+
+else{
+  document.write("<p>" +"This was your answer: " + answer1+ "...WRONG!" + "</p>");
+}
+
+
+if ( answer2.toUpperCase() === 'TRANSPORT LAYER' ) {
+ correct += 1;
+ document.write("<p>" +"This was your answer: " + answer2+ "...GOOD LOOKS! that was correct"+"</p>");
+}
+
+else{
+  document.write("<p>" +"This was your answer: " + answer2+ "...WRONG!" + "</p>");
+}
+
+if ( answer3.toUpperCase() === 'TRANSPORT LAYER' ) {
+ correct += 1;
+ document.write("<p>" +"This was your answer: " + answer3+ "...GOOD LOOKS! that was correct"+"</p>");
+}
+
+else{
+  document.write("<p>" +"This was your answer: " + answer3+ "...WRONG!" + "</p>");
+}
+
+if ( answer4.toUpperCase() === 'TCP' ) {
+ correct += 1;
+ document.write("<p>" +"This was your answer: " + answer4+ "...GOOD LOOKS! that was correct"+"</p>");
+}
+
+else{
+  document.write("<p>" +"This was your answer: " + answer4+ "...WRONG!" + "</p>");
+}
+
+if ( answer5.toUpperCase() === 'GETINPUTSTREAM AND CLOSE' ) {
+ correct += 1;
+ document.write("<p>" +"This was your answer: " + answer5+ "...GOOD LOOKS! that was correct"+"</p>");
+}
+
+else{
+  document.write("<p>" +"This was your answer: " + answer5+ "...WRONG!" + "</p>");
+}
+
+
+document.write("<P>"+ "You Got " + correct + " out of 5 answers correct. Shoot me an email if you'd like to discuss your results.");
+document.write("<P>" + "HAVE A NICE DAY!"+ "</P>");
